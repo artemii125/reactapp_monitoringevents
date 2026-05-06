f51cd4c1353e34cd5a1eec46f880c3ee222a1f47
artemii125 <larind8@gmail.com>
2026-05-07 01:52:17 +0700

    Fix useState import

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
