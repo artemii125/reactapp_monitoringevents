172c94e917b9537f452b20eff0fdb77fea3805a6
artemii125 <larind8@gmail.com>
2026-05-08 16:29:09 +0700

    Fix mail send interface 2

--- api/send-email.js
+++ api/send-email.js
@@ -20,21 +20,20 @@ module.exports = async (req, res) => {
 
   try {
     await transporter.sendMail({
-      from: '"Система Мониторинга" <noreply@security.ru>',
+      from: '"Система Мониторинга внутреннних и внешних мероприятий"',
       to: "larin.2024@stud.nstu.ru",
-      subject: isDeletion ? `УВЕДОМЛЕНИЕ: УДАЛЕНИЕ ОБЪЕКТА - ${name}` : `УВЕДОМЛЕНИЕ: РЕГИСТРАЦИЯ ОБЪЕКТА - ${name}`,
+      subject: isDeletion ? `УДАЛЕНИЕ ОБЪЕКТА - ${name}` : `РЕГИСТРАЦИЯ ОБЪЕКТА - ${name}`,
       html: `
         <div style="max-width: 600px; margin: 20px auto; font-family: 'Courier New', Courier, monospace; color: #000; border: 1px solid #000; padding: 20px;">
-          <h2 style="text-transform: uppercase; border-bottom: 1px solid #000; padding-bottom: 10px;">
-            ${isDeletion ? 'Отчет об удалении' : 'Отчет о регистрации'}
+          <h2 style="text-transform: uppercase; padding-bottom: 10px;">
+            ${isDeletion ? 'Удалено мероприятие' : 'Зарегестрировано новое мероприятие'}
           </h2>
-          <div style="margin: 20px 0;">
             <p><strong>Наименование:</strong> ${name}</p>
             <p><strong>Местоположение:</strong> ${venue}</p>
             <p><strong>Класс защиты:</strong> ${security}</p>
             <p><strong>Тип операции:</strong> ${isDeletion ? 'Исключение из реестра' : 'Внесение в реестр'}</p>
           </div>
-          <p style="font-size: 11px; border-top: 1px solid #000; pt: 10px;">
+
             Системное время: ${formattedDate}
           </p>
         </div>
